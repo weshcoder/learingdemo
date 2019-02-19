76fbb05b15f72d8e4b43baabbc426965e77f302c
weshcoder <wegairungum21@gmail.com>
2019-02-19 07:20:12 +0300

    modified

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return ("Hello World")
+	return ("Flask is complete")
 
 if __name__ == '__main__':
 	app.run()
