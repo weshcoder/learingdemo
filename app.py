843e71b3d081584b7ddc552b9caac63c72f07a51
weshcoder <wegairungum21@gmail.com>
2019-02-19 07:33:08 +0300

    modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-	return ("Flask is complete")
+	return ("Flask is complete!!!! Yay!")
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True, port=3000)
