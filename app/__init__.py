71acbc0e1a3bb6a7d629aa91367ee4b81325759a
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-04-17 18:32:13 +0300

    Fix registration form bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-from flask  import Flask, render_template, flash, redirect, request
+from flask import Flask, render_template, flash, redirect, request, url_for
 from .models import db, Users
-from .forms import RegistrationForm, LoginForm 
+from .forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] =  'bee3d7c48f45083e800d8a505c396a6b'
+app.config['SECRET_KEY'] = 'bee3d7c48f45083e800d8a505c396a6b'
 app.config['DEBUG'] = True
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://DB_USER:PASSWORD@HOST/DATABASE'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:marthairungu123#@localhost/Flask-Bucket-list'
 POSTGRES = {
     'user': 'postgres',
     'pw': 'marthairungu123#',
@@ -16,47 +16,48 @@ POSTGRES = {
     'host': 'localhost',
     'port': '5432',
 }
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route("/about")
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route("/login")
 def login():
-  form = LoginForm()
-  return render_template("login.html", title='Login', form = form) 
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-  form = RegistrationForm()
+    form = RegistrationForm(request.form)
 
-  if request.method == 'POST':
-    print("\n\n" + str(request.data.decode()) + "\n\n")
+    if request.method == 'POST' and form.validate():
+        username = form.Username.data
+        email = form.Email.data
+        password = form.Password.data
 
-    username = str(request.data.get('username', ''))
-    email = str(request.data.get('email', ''))
-    password = str(request.data.get('password', ''))
+        user = Users(username=username, email=email, password=password)
+        user.save()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
 
-    if form.validate_on_submit():
-      flash(f'Account created for {form.username.data}!', 'success')
+    return render_template("register.html", title='Register', form=form)
 
-      user = Users(username=username, email=email, password=password)
-      user.save()
 
-  return render_template("register.html", title='Register', form = form)
 @app.route("/trial")
 def trial():
-  return render_template("trial.html")
+    return render_template("trial.html")
+
 
 @app.route('/logged')
 def logged():
-  return render_template('logged.html')
-    
+    return render_template('logged.html')
--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,17 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import Form, StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(Form):
     Username = StringField('Username', render_kw={"class": "form-control", "placeholder":'Enter Username'}, validators=[DataRequired(), Length(min =2, max = 20)])
     Email = StringField('Email', render_kw={"class": "form-control", "placeholder":'Enter Email'}, validators=[DataRequired(), Email()])
-    Password = PasswordField('Password',render_kw={"class": "form-control", "placeholder":'Enter Password'},validators=[DataRequired()])
-    Repeat_Password = PasswordField('Repeat_Password',render_kw={"class": "form-control", "placeholder":'Repeat Password'}, validators=[DataRequired(), EqualTo('Password')])
+    Password = PasswordField('Password', render_kw={"class": "form-control", "placeholder":'Enter Password'}, validators=[DataRequired()])
+    Repeat_Password = PasswordField('Repeat_Password', render_kw={"class": "form-control", "placeholder":'Repeat Password'}, validators=[DataRequired(), EqualTo('Password')])
     Submit = SubmitField('Sign Up')
 
-class LoginForm(FlaskForm):
-    Email = StringField('Email', validators=[DataRequired(), Email()])
-    Password = PasswordField('Password', validators=[DataRequired()])
+class LoginForm(Form):
+    Email = StringField('Email', render_kw={"class": "form-control", "placeholder":'Enter Email'}, validators=[DataRequired(), Email()])
+    Password = PasswordField('Password', render_kw={"class": "form-control", "placeholder":'Enter Password'}, validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     Submit = SubmitField('Login')   
 
